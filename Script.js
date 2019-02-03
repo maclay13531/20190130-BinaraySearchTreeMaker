44380b6fe16346e0db82043f517cee37fdc0eaa7
Jong Park <maclay13531@gmail.com>
2019-02-03 17:41:05 -0500

    add - user input value insert / add - binary nodes classes

--- Script.js
+++ Script.js
@@ -1,48 +1,50 @@
-// create a binary search tree
-class NewNode {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-class BinarySearchTree {
-    constructor() {
-        this.root = null;
+$(document).ready(function () {
+    // create a binary search tree and its new node(s)
+    class NewNode {
+        constructor(value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
     }
 
-    insert(value) {
-        const newNode = new NewNode(value);
-
-        if (!this.root) {
-            this.root = newNode;
-            return this;
+    class BinarySearchTree {
+        constructor() {
+            this.root = null;
         }
 
-        let currentNodeToCheck = this.root;
-        while (currentNodeToCheck) {
-            if (currentNodeToCheck.value > value) {
-                if (!currentNodeToCheck.left) {
-                    currentNodeToCheck.left = newNode;
-                    return this;
+        insert(value) {
+            const newNode = new NewNode(value);
+
+            if (!this.root) {
+                this.root = newNode;
+                return this;
+            }
+
+            let currentNodeToCheck = this.root;
+            while (currentNodeToCheck) {
+                if (currentNodeToCheck.value > value) {
+                    if (!currentNodeToCheck.left) {
+                        currentNodeToCheck.left = newNode;
+                        return this;
+                    }
+                    currentNodeToCheck = currentNodeToCheck.left;
+                } else {
+                    if (!currentNodeToCheck.right) {
+                        currentNodeToCheck.right = newNode;
+                        return this;
+                    }
+                    currentNodeToCheck = currentNodeToCheck.right;
                 }
-                currentNodeToCheck = currentNodeToCheck.left;
-            } else {
-                if (!currentNodeToCheck.right) {
-                    currentNodeToCheck.right = newNode;
-                    return this;
-                }
-                currentNodeToCheck = currentNodeToCheck.right;
             }
         }
     }
-}
 
-let myBinarySearchTree = new BinarySearchTree();
-myBinarySearchTree.insert(20);
-myBinarySearchTree.insert(25);
-myBinarySearchTree.insert(30);
-myBinarySearchTree.insert(15);
-myBinarySearchTree.insert(23);
-console.log(myBinarySearchTree);
+    let userBinarySearchTree = new BinarySearchTree();
+
+    $("#insert-number").click(function () {
+        let userInsertedNumber = $("#user-insert-number").val();
+        userBinarySearchTree.insert(userInsertedNumber);
+    })
+});
+
